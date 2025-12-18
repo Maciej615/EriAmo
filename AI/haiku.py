d833d9397a46901bc02800130f98fba8075a2085
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 19:59:30 +0100

    Add copyright and license comments to haiku.py
    Added copyright and licensing information to haiku.py

--- AI/haiku.py
+++ AI/haiku.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 # haiku.py - Generator Haiku dla EriAmo
+# Copyright (C) 2025 Maciej Mazur (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 """
 Moduł twórczy: Generowanie haiku na podstawie stanu emocjonalnego.
 
