326713fb94dce98981f884c36abb9b49cf38031d
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 19:55:38 +0100

    Add copyright and license information to byt.py
    Added copyright notice and licensing information.

--- AI/byt.py
+++ AI/byt.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 # byt.py
+# Copyright (C) 2025 Maciek (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 import numpy as np
 
 class BytS:
