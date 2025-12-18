5d6021909a2a36e70b6dabbca623edffab5c5e9e
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 19:55:13 +0100

    Add copyright and license information to aii.py
    Added copyright notice and license information.

--- AI/aii.py
+++ AI/aii.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # aii.py (v5.3.0-Evolution) - Pełna Autonomia i Fusion
-
+# Copyright (C) 2025 Maciek (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 import numpy as np
 import time
 import threading
