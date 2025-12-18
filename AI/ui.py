a9d298b26a10925ed4b3bca2bc55a2961bb8698e
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 20:01:16 +0100

    Add copyright and license to ui.py
    Added copyright notice and license information.

--- AI/ui.py
+++ AI/ui.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 # ui.py
+# Copyright (C) 2025 Maciej Mazur (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 import sys
 import time
 from config import Colors
