c91aa303fab8f8334219f3357db6ae5026b73d3e
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 19:58:39 +0100

    Add copyright and license details to genesiskit.py
    Added copyright and licensing information to the file.

--- AI/genesiskit.py
+++ AI/genesiskit.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 # genesis_gramatyka.py - Rozbudowany Genesis z Szkieletami Gramatycznymi
 # Setki naturalnych przykładów dla każdej emocji - fundament dla przyszłych instalacji
+# Copyright (C) 2025 Maciej Mazur (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
 from aii import AII
 from config import Colors
