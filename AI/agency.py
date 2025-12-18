a7921d2c6aadc2792e7952c227a054df611882e0
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 19:54:55 +0100

    Add copyright and license details to agency.py
    Added copyright notice and licensing information.

--- AI/agency.py
+++ AI/agency.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 # agency.py - Autonomiczna Agencja Twórcza dla EriAmo
+# Copyright (C) 2025 Maciek (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 """
 Moduł zarządzający autonomicznymi działaniami EriAmo
 gdy system się nudzi (brak interakcji użytkownika).
