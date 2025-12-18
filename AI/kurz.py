84c99cc1020f351abd5e0024615dc828c7886933
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 19:59:45 +0100

    Update kurz.py

--- AI/kurz.py
+++ AI/kurz.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 # kurz.py (v5.2.0-Fusion) - Warstwa Odruchowa
+# Copyright (C) 2025 Maciej Mazur (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
 import re
 from config import Colors
