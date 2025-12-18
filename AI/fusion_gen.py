e2bba302f5b2a969372c0fbd5fc7a45c30b9f010
Maciej <107417669+Maciej615@users.noreply.github.com>
2025-12-18 19:57:45 +0100

    Add copyright and license to fusion_gen.py
    Added copyright notice and license information.

--- AI/fusion_gen.py
+++ AI/fusion_gen.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # fusion_gen.py - Naprawiony proces budowania matrycy
-
+# Copyright (C) 2025 Maciej Mazur (maciej615)
+# EriAmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 import json
 import os
 from aii import AII
